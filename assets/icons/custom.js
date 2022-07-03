25c71929a9c3bdb26065d22265e4656b280dae84
Andy Costanza <andy.costanza@pm.me>
2022-07-03 14:27:24 +0200

    fix: changing theme

--- assets/icons/custom.js
+++ assets/icons/custom.js
@@ -1,10 +1,12 @@
-import { faClock, faNewspaper } from '@fortawesome/free-solid-svg-icons';
-import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
-import { faAmazon, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import {
+    faBook, faBug, faHeart, faImage, faImages, faNewspaper,
+} from '@fortawesome/free-solid-svg-icons';
+// import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
+// import { faAmazon, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const icons = [
-    faClock,
-    faAddressBook, faNewspaper ,
-    faAmazon, faGoogle,
+    faBook, faBug, faHeart, faImage, faImages, faNewspaper,
+    // faAddressBook,
+    // faAmazon, faGoogle,
 ];
 export default icons;
